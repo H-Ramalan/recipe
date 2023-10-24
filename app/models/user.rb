0ca6827d0e23ada64150d9affc4d6d1b125a4275
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-24 14:44:26 +0500

    Added tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :foods, foreign_key: :user_id, dependent: :destroy
 
   validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
