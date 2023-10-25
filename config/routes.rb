5258ff9a5ff394ab6e2cb4b6eb0b420d64717732
H-Ramalan <talktoramalan@gmail.com>
2023-10-25 10:41:23 -0700

    Bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :users
+
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
