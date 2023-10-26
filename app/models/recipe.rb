3e2f09715ddc607510e2820168b6f7ef2deeaf30
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-26 10:04:15 +0500

    Update recipe and food models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods, foreign_key: :recipe_id
+  has_many :recipe_foods, foreign_key: :recipe_id, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :preparation_time, presence: true
